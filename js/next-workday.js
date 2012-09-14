d1481d6501543a37e07e6b9cc0900d3ef83d5f48
Morgan Estes <morgan.estes@gmail.com>
2012-09-13 23:16:03 -0500

    Added async loading via Prototype Ajax.Request()

--- js/next-workday.js
+++ js/next-workday.js
@@ -1,29 +1,18 @@
-// date.js implementation
-var workdays = ["Jan. 22", 
-                "Feb. 12", 
-                "Mar. 4", 
-                "Apr. 1", 
-                "May 6", 
-                "June 3", 
-                "July 15", 
-                "Aug. 5", 
-                "Sept. 9", 
-                "Oct. 7", 
-                "Nov. 4", 
-                "Dec. 9"],
-    today = Date.today(),
-    month = today.getMonth(),
-    workday = Date.parse(workdays[month]),
-    next = function() {
-        var _next;
-        if (today.compareTo(workday) <= 0) {
-            _next = workdays[month];
-        } else {
-            _next = workdays[month + 1];
-        }
-        //return _next;
+// Since Weebly already uses Prototype, we'll take advantage of their library
+var dateUrl = '//threadsokc.github.com/js/date.js';
+new Ajax.Request(dateUrl, {
+    onSuccess: showNextWorkday
+    });
 
-        return (today.compareTo(workday) <=0) ? workdays[month] : workdays[month+1];
-    };
-    
-document.getElementById("next-workday").getElementsByTagName("span")[0].innerHTML = next();
+function showNextWorkday() {
+    // date.js implementation
+    var workdays = ["Jan. 22", "Feb. 12", "Mar. 4", "Apr. 1", "May 6", "June 3", "July 15", "Aug. 5", "Sept. 9", "Oct. 7", "Nov. 4", "Dec. 9"],
+        today = Date.today(),
+        month = today.getMonth(),
+        workday = Date.parse(workdays[month]),
+        next = function() {
+            return (today.compareTo(workday) <= 0) ? workdays[month] : workdays[month + 1];
+        };
+
+    document.getElementById('next-workday').getElementsByTagName('span')[0].innerHTML = next();
+}
