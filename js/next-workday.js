87fb0a75758f48fef1dea146ab8f8b15bdf494d3
Morgan Estes <morgan.estes@gmail.com>
2012-09-13 21:33:51 -0500

    Tightened up return call in next()

--- js/next-workday.js
+++ js/next-workday.js
@@ -1,5 +1,16 @@
 // date.js implementation
-var workdays = ["Jan. 22", "Feb. 12", "Mar. 4", "Apr. 1", "May 6", "June 3", "July 15", "Aug. 5", "Sept. 9", "Oct. 7", "Nov. 4", "Dec. 9"],
+var workdays = ["Jan. 22", 
+                "Feb. 12", 
+                "Mar. 4", 
+                "Apr. 1", 
+                "May 6", 
+                "June 3", 
+                "July 15", 
+                "Aug. 5", 
+                "Sept. 9", 
+                "Oct. 7", 
+                "Nov. 4", 
+                "Dec. 9"],
     today = Date.today(),
     month = today.getMonth(),
     workday = Date.parse(workdays[month]),
@@ -10,6 +21,9 @@ var workdays = ["Jan. 22", "Feb. 12", "Mar. 4", "Apr. 1", "May 6", "June 3", "Ju
         } else {
             _next = workdays[month + 1];
         }
-        return _next;
+        //return _next;
+
+        return (today.compareTo(workday) <=0) ? workdays[month] : workdays[month+1];
     };
+    
 document.getElementById("next-workday").getElementsByTagName("span")[0].innerHTML = next();
