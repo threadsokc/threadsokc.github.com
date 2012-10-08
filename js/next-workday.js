418ceadce10a7d6c163220e0fe2641734c38d8a6
Morgan Estes <morgan.estes@gmail.com>
2012-10-07 21:32:48 -0500

    Update js/next-workday.js

--- js/next-workday.js
+++ js/next-workday.js
@@ -29,8 +29,9 @@ Threads.showNextWorkDay = function() {
 Threads.init = function() {
     var dateUrl = '//threadsokc.github.com/js/date.js';
     jQuery.getScript(dateUrl, function() {
-        Threads.showNextWorkday();
+        console.log('date.js loaded');
     });
+    Threads.showNextWorkday();
 };
 
 jQuery(function() {
