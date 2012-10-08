3fea60b9b44ffff9117d2e11f7a494cad42d2549
Morgan Estes <morgan.estes@gmail.com>
2012-10-07 21:40:15 -0500

    Update js/next-workday.js

--- js/next-workday.js
+++ js/next-workday.js
@@ -2,14 +2,14 @@
  * Wrapper for ThreadsOKC.org scripts. 
  * @namespace
  */
-var Threads = Threads || {};
+var Threads = Threads || {
 
 /**
  * Gets the next workday and displays it on screen.
  * @requires date.js
  * @returns String Short month and day. 
  */
-Threads.showNextWorkDay = function() {
+showNextWorkDay: function() {
     var dateUrl = '//threadsokc.github.com/js/date.js',
         workdays = ["Jan. 22", "Feb. 12", "Mar. 4", "Apr. 1", "May 6", "June 3", "July 15", "Aug. 5", "Sept. 9", "Oct. 7", "Nov. 4", "Dec. 9"],
         today = Date.today(),
@@ -24,14 +24,16 @@ Threads.showNextWorkDay = function() {
         //document.getElementById('next-workday').getElementsByTagName('span')[0].innerHTML = next();
     });
     
-};
+},
 
 /**
  * Initializer for Threads.
  * @requires jQuery 
  */
-Threads.init = function() {
+init: function() {
     Threads.showNextWorkday();
+};
+
 };
 
 jQuery(function() {
