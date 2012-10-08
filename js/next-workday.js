c5b688b66ea0e8c1b78d8241145249297a9b459d
Morgan Estes <morgan.estes@gmail.com>
2012-10-07 20:29:42 -0500

    modified:   next-workday.js 	Namespaced Threads and added .init()

--- js/next-workday.js
+++ js/next-workday.js
@@ -1,10 +1,24 @@
-// Weebly loads jQuery, too. Cheaters!
-var dateUrl = '//threadsokc.github.com/js/date.js';
-jQuery.getScript(dateUrl, showNextWorkday);
+/**
+ * Wrapper for ThreadsOKC.org scripts. 
+ * @namespace
+ */
+var Threads = Threads || {};
 
+/**
+ * Initializer for Threads.
+ * @requires jQuery 
+ */
+Threads.init = function init() {
+    var dateUrl = '//threadsokc.github.com/js/date.js';
+    jQuery.getScript(dateUrl, Threads.showNextWorkday);
+};
 
-function showNextWorkday() {
-    // date.js implementation
+/**
+ * Gets the next workday and displays it on screen.
+ * @requires date.js
+ * @returns String Short month and day. 
+ */
+Threads.showNextWorkDay = function showNextWorkday() {
     var workdays = ["Jan. 22", "Feb. 12", "Mar. 4", "Apr. 1", "May 6", "June 3", "July 15", "Aug. 5", "Sept. 9", "Oct. 7", "Nov. 4", "Dec. 9"],
         today = Date.today(),
         month = today.getMonth(),
@@ -13,5 +27,6 @@ function showNextWorkday() {
             return (today.compareTo(workday) <= 0) ? workdays[month] : workdays[month + 1];
         };
 
-    document.getElementById('next-workday').getElementsByTagName('span')[0].innerHTML = next();
-}
+    $('#next-workday span').html(next());
+    //document.getElementById('next-workday').getElementsByTagName('span')[0].innerHTML = next();
+};
