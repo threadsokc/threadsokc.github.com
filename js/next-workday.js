9fe6ce4970da7c7ad665536cf6dd466020157b64
Morgan Estes <morgan.estes@gmail.com>
2012-10-07 21:50:42 -0500

    Update js/next-workday.js

--- js/next-workday.js
+++ js/next-workday.js
@@ -10,8 +10,7 @@ var Threads = Threads || {};
  * @returns String Short month and day. 
  */
 Threads.showNextWorkDay = function showNextWorkDay() {
-    var dateUrl = '//threadsokc.github.com/js/date.js',
-        workdays = ["Jan. 22", "Feb. 12", "Mar. 4", "Apr. 1", "May 6", "June 3", "July 15", "Aug. 5", "Sept. 9", "Oct. 7", "Nov. 4", "Dec. 9"],
+    var workdays = ["Jan. 22", "Feb. 12", "Mar. 4", "Apr. 1", "May 6", "June 3", "July 15", "Aug. 5", "Sept. 9", "Oct. 7", "Nov. 4", "Dec. 9"],
         today = Date.today(),
         month = today.getMonth(),
         workday = Date.parse(workdays[month]),
@@ -19,11 +18,9 @@ Threads.showNextWorkDay = function showNextWorkDay() {
             return (today.compareTo(workday) <= 0) ? workdays[month] : workdays[month + 1];
         };
 
-    jQuery.getScript(dateUrl, function() {
+    
         jQuery('#next-workday span').html(next());
         //document.getElementById('next-workday').getElementsByTagName('span')[0].innerHTML = next();
-    });
-    
 };
 
 /**
@@ -31,7 +28,11 @@ Threads.showNextWorkDay = function showNextWorkDay() {
  * @requires jQuery 
  */
 Threads.init = function init() {
-    return Threads.showNextWorkDay();
+    var dateUrl = '//threadsokc.github.com/js/date.js';
+    
+    jQuery.getScript(dateUrl, function() {
+        Threads.showNextWorkDay();
+    });
 };
 
 
