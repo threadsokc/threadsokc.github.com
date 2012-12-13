c1abe6816aa0346d47789bf9dd4af0bd4e848802
Morgan Estes <morgan.estes@gmail.com>
2012-12-13 16:35:10 -0600

    Update js/next-workday.js

--- js/next-workday.js
+++ js/next-workday.js
@@ -15,7 +15,7 @@ Threads.showNextWorkDay = function showNextWorkDay() {
         month = today.getMonth(),
         workday = Date.parse(workdays[month]),
         next = function () {
-            return (today.compareTo(workday) <= 0) ? workdays[month] : workdays[month + 1];
+            return today.compareTo(workday) <= 0 ? workdays[month] : workdays[month + 1];
         };
 
     $('#next-workday span').html(next());
