68b91514e974457b355fe96b061bef5202622c74
Morgan Estes <morgan.estes@gmail.com>
2012-12-13 16:08:00 -0600

    Updated dates for 2013.

--- js/next-workday.js
+++ js/next-workday.js
@@ -1,5 +1,5 @@
 /**
- * Wrapper for ThreadsOKC.org scripts. 
+ * Wrapper for ThreadsOKC.org scripts.
  * @namespace
  */
 var Threads = Threads || {};
@@ -7,35 +7,33 @@ var Threads = Threads || {};
 /**
  * Gets the next workday and displays it on screen.
  * @requires date.js
- * @returns String Short month and day. 
+ * @returns String Short month and day.
  */
 Threads.showNextWorkDay = function showNextWorkDay() {
-    var workdays = ["Jan. 22", "Feb. 12", "Mar. 4", "Apr. 1", "May 6", "June 3", "July 15", "Aug. 5", "Sept. 9", "Oct. 7", "Nov. 4", "Dec. 9"],
+    var workdays = ["Jan. 13", "Feb. 3", "Mar. 3", "Apr. 17", "May 5", "June 2", "July TBD", "Aug. TBD", "Sept. TBD", "Oct. TBD", "Nov. TBD", "Dec. TBD"],
         today = Date.today(),
         month = today.getMonth(),
         workday = Date.parse(workdays[month]),
-        next = function() {
-            return (today.compareTo(workday) <= 0) ? workdays[month] : workdays[month + 1];
+        next = function () {
+            return(today.compareTo(workday) <= 0) ? workdays[month] : if (month == 11) { workdays[1] } else { workdays[month + 1] };
         };
 
-    
-        jQuery('#next-workday span').html(next());
-        //document.getElementById('next-workday').getElementsByTagName('span')[0].innerHTML = next();
+    jQuery('#next-workday span').html(next());
+    //document.getElementById('next-workday').getElementsByTagName('span')[0].innerHTML = next();
 };
 
 /**
  * Initializer for Threads.
- * @requires jQuery 
+ * @requires jQuery
  */
 Threads.init = function init() {
     var dateUrl = '//threadsokc.github.com/js/date.js';
-    
-    jQuery.getScript(dateUrl, function() {
+
+    jQuery.getScript(dateUrl, function () {
         Threads.showNextWorkDay();
     });
 };
 
-
-jQuery(function() {
+jQuery(function () {
     Threads.init();
 });
